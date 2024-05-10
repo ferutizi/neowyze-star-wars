19070327e4d04bba5da4eaf64bbd2bc88ba1baa8
ferutizi <fer.utizi@gmail.com>
2024-05-10 19:40:17 -0300

    add(-): added characters page and mapped characters

---
+++ src/app/characters/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { getCharacters } from "../api";
+import { CharacterType } from "../types";
+
+export default async function Characters () {
+  const characters: CharacterType[] = await getCharacters()
+
+  return(
+    <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
+        {
+          characters.map((character) => 
+            // Gets id from the end of the url
+            <Link href={`characters/${character.url.replace("https://swapi.dev/api/people/", "").replace("/", "")}`} key={character.name}>
+              <article>
+                <h2>{character.name}</h2>
+                {/* img */}
+                <p>{character.eye_color}</p>
+                <p>{character.gender}</p>
+              </article>
+            </Link>
+          )
+        }
+      </section>
+  )
+}
