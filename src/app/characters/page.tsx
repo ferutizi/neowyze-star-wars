3b5ef72f2e623ce4d1119e5e62eeb197828d070f
ferutizi <fer.utizi@gmail.com>
2024-05-11 14:53:18 -0300

    refactor(-): created function getIdFromUrl

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
 import { getCharacters } from "../api";
+import { CharacterType } from "../types";
 
 export default async function Characters () {
   const characters = await getCharacters()
+
+  const getIdFromUrl = (url: CharacterType["url"]): string => {
+    const id = url.replace("https://swapi.dev/api/people/", "").replace("/", "")
+    return id
+  }
 
   return(
     <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 p-14">
       {
         characters.map((character) => 
           // Gets id from the end of the url
-          <Link href={`characters/${character.url.replace("https://swapi.dev/api/people/", "").replace("/", "")}`} key={character.name}>
+          <Link href={`characters/${getIdFromUrl(character.url)}`} key={character.name}>
             <article>
               <h2>{character.name}</h2>
               <img src="/character.png" alt={character.name} width={400} height={200} />
