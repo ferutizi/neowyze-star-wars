a8e2086d2f6fe79433618eb2e72862eb73d31131
ferutizi <fer.utizi@gmail.com>
2024-05-11 00:06:15 -0300

    fix(-): added padding in section tag

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -5,7 +5,7 @@ export default async function Characters () {
   const characters = await getCharacters()
 
   return(
-    <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
+    <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 p-14">
       {
         characters.map((character) => 
           // Gets id from the end of the url
