e820cb772b0503bb7dacba91f27a6c748037a2a3
ferutizi <fer.utizi@gmail.com>
2024-05-11 00:04:15 -0300

    fix(-): removing vertical scroll bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Films from "./components/Films";
 export default async function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-14">
       <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
         <Films />
       </section>
