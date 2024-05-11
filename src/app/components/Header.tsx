6efbca61b285b5d1293e79d421f22609fc49c830
ferutizi <fer.utizi@gmail.com>
2024-05-11 00:45:31 -0300

    fix(-): changed max width component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 export default function Header() {
   return(
     <header className="flex items-center justify-center pl-14">
-      <div className="flex items-center max-w-screen-xl w-full">
+      <div className="flex items-center max-w-[1920px] w-full">
         <img src="/logo.png" alt="Star Wars logo" width={120} height={100} />
         <Nav />
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between p-14">
-      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
+      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 max-w-[1920px]">
         <Films />
       </section>
     </main>
