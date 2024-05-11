2007288eac5e9870cfcf1e9e0bef7465d25ae1d0
ferutizi <fer.utizi@gmail.com>
2024-05-11 14:50:38 -0300

    fix(-): id did not match episode number. the getIdFromUrl function was created

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -3,14 +3,18 @@ import { FilmType } from "../types"
 
 export default async function FilmPage({params: {id}}: {params: {id: number}}) {
   const film: FilmType = await getFilm(id)
-
+  const getIdFromUrl = (url: FilmType["url"]): string => {
+    const id = url.replace("https://swapi.dev/api/films/", "").replace("/", "")
+    return id
+  }
+  
   return(
     <>
       {film &&
         <article>
           <h1>{film.title}</h1>
           {/* img */}
-          <p>Episodio: {film.episode_id}</p>
+          <p>Episodio: {getIdFromUrl(film.url)}</p>
           <p>{film.director}</p>
         </article>
       }
--- src/app/components/Films.tsx
+++ src/app/components/Films.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link"
 import { getFilms } from "../api"
 import Image from "next/image"
+import { FilmType } from "../types"
 
 export default async function Films() {
   const films = await getFilms()
+  const getIdFromUrl = (url: FilmType["url"]): string => {
+    const id = url.replace("https://swapi.dev/api/films/", "").replace("/", "")
+    return id
+  }
+
   return(
     <>
       {
         films.map(film => 
-          <Link href={`/${film.episode_id}`} key={film.episode_id}>
+          <Link href={`/${getIdFromUrl(film.url)}`} key={film.episode_id}>
             <article className="">
               <h2>{film.title}</h2>
               <Image src="/film-banner.jpg" alt={`Banner ${film.title}`} width={1200} height={600} />
-              <p>Episodio {film.episode_id}</p>
+              <p>Episodio {getIdFromUrl(film.url)}</p>
             </article>
           </Link>
         )
