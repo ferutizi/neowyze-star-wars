c53e8480b76ee49a418e86852be7777ffa625538
ferutizi <fer.utizi@gmail.com>
2024-05-11 16:00:52 -0300

    fix(-): if path includes characters, isCharacterPage

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,19 +5,19 @@ import { usePathname } from "next/navigation"
 
 export default function Nav() {
   const path = usePathname()
-  const isHomePage = path === "/"
+  const isCharacterPage = path.includes("characters")
 
-  return(
+  return (
     <nav className="flex m-auto justify-center text-lg -translate-x-[120px] border-2 border-primary rounded-full text-slate-300">
       <Link href="/">
         <button
-          className={`${isHomePage ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
+          className={`${!isCharacterPage ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
         >Películas
         </button>
       </Link>
       <Link href="/characters">
         <button
-          className={`${!isHomePage ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
+          className={`${isCharacterPage ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
         >Personajes
         </button>
       </Link>
