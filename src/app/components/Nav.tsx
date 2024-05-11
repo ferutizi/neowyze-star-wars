f6ab29f6b8acd8da9ef95d2e0247976178ce450a
ferutizi <fer.utizi@gmail.com>
2024-05-11 00:45:14 -0300

    add(-): style switch nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -10,14 +10,14 @@ export default function Nav() {
     <nav className="flex m-auto justify-center text-lg -translate-x-[120px] border-2 border-primary rounded-full text-slate-300">
       <Link href="/">
         <button
-          className={`${isFilmOn ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-48 py-2 transition-all ease-in-out duration-200`}
+          className={`${isFilmOn ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
           onClick={() => setIsFilmOn(true)}
         >Películas
         </button>
       </Link>
       <Link href="/characters">
         <button
-          className={`${!isFilmOn ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-48 py-2 transition-all ease-in-out duration-200`}
+          className={`${!isFilmOn ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
           onClick={() => setIsFilmOn(false)}
         >Personajes
         </button>
