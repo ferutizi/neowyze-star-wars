fb09d3dd42cdf9595e9a44536d14cf88c71f9b00
ferutizi <fer.utizi@gmail.com>
2024-05-11 01:02:44 -0300

    add(-): redirect to home from logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import Nav from "./Nav";
 
 export default function Header() {
   return(
     <header className="flex items-center justify-center pl-14">
       <div className="flex items-center max-w-[1920px] w-full">
-        <img src="/logo.png" alt="Star Wars logo" width={120} height={100} />
+        <Link href="/">
+          <img src="/logo.png" alt="Star Wars logo" width={120} height={100} />
+        </Link>
         <Nav />
       </div>
     </header>
