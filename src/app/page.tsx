3ca35650735ac185b6c1f7631849b2061d80a284
ferutizi <fer.utizi@gmail.com>
2024-05-10 22:25:21 -0300

    drop(-): films moved to new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
-import Link from "next/link"
-import { getFilms } from "./api"
-import { FilmType } from "./types"
+import Films from "./components/Films";
 
 export default async function Home() {
-  const films: FilmType[] = await getFilms()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
-        {
-          films.map(film => 
-            <Link href={`/${film.episode_id}`} key={film.episode_id}>
-              <article>
-                <h2>{film.title}</h2>
-                {/* img */}
-                <p>Episodio {film.episode_id}</p>
-              </article>
-            </Link>
-          )
-        }
+        <Films />
       </section>
     </main>
   );
