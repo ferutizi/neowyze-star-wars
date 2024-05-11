a30c97af4c35dde81087166f9e91e40099251b93
ferutizi <fer.utizi@gmail.com>
2024-05-11 01:04:17 -0300

    refactor(-): now usePathname is used to handle switch state

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,24 +1,23 @@
 'use client'
 
 import Link from "next/link"
-import { useState } from "react"
+import { usePathname } from "next/navigation"
 
 export default function Nav() {
-  const [isFilmOn, setIsFilmOn] = useState<boolean>(true)
+  const path = usePathname()
+  const isHomePage = path === "/"
 
   return(
     <nav className="flex m-auto justify-center text-lg -translate-x-[120px] border-2 border-primary rounded-full text-slate-300">
       <Link href="/">
         <button
-          className={`${isFilmOn ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
-          onClick={() => setIsFilmOn(true)}
+          className={`${isHomePage ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
         >Películas
         </button>
       </Link>
       <Link href="/characters">
         <button
-          className={`${!isFilmOn ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
-          onClick={() => setIsFilmOn(false)}
+          className={`${!isHomePage ? "bg-primary text-black" : "hover:text-primary"} font-bold rounded-full w-44 py-2 transition-all ease-in-out duration-200`}
         >Personajes
         </button>
       </Link>
