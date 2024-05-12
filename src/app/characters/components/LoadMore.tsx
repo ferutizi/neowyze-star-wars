3813350a032e6a03f5a959b52cd9eeb109120796
ferutizi <fer.utizi@gmail.com>
2024-05-12 18:54:51 -0300

    add(-): filter by eye color and/or gender. filter is hardcoded

--- src/app/characters/components/LoadMore.tsx
+++ src/app/characters/components/LoadMore.tsx
@@ -2,14 +2,18 @@
 
 import { useInView } from "react-intersection-observer"
 import { useState, useEffect } from "react"
-import { CharacterType } from "@/app/types"
+import { CharacterType, FiltersType } from "@/app/types"
 import { getCharacters } from "@/app/api"
 import Link from "next/link"
 import { getIdFromUrl } from "@/app/hooks/useGetIdFromUrl"
 
+interface LoadMoreProps {
+  filters: FiltersType
+}
+
 let page = 2;
 
-export default function LoadMore() {
+export default function LoadMore({ filters }: LoadMoreProps) {
   const [characters, setCharacters] = useState<CharacterType[]>([])
   const { ref, inView } = useInView()
 
@@ -25,32 +29,37 @@ export default function LoadMore() {
   return (
     <>
       {
-        characters.map(({ name, url, gender, eye_color }) =>
-          <article key={name} className="flex flex-col items-center">
-            <Link href={`characters/${getIdFromUrl({ query: { url: url, q: "people" } })}`}>
-              <h2 className="text-2xl pl-2 text-primary font-bold">{name}</h2>
-              <img className="rounded-lg" src="/character.webp" alt={name} width={400} height={200} />
-              <div className="flex justify-around">
-                {
-                  gender !== "unknown" &&
-                  gender !== "n/a" &&
-                  <div className="flex items-center gap-4">
-                    <p className="text-xl py-2">{gender}</p>
-                    <img className="h-4" src={`/${gender}.webp`} alt="eye" height={'auto'} />
-                  </div>
-                }
-                {
-                  eye_color !== "unknown" &&
-                  eye_color !== "n/a" &&
-                  <div className="flex items-center gap-4">
-                    <img className={`h-6 eye ${"eye-" + eye_color}`} src="eye.webp" alt="eye" height={'auto'} />
-                    <p className="text-xl py-2">{eye_color}</p>
-                  </div>
-                }
-              </div>
-            </Link>
-          </article>
-        )
+        characters
+          .filter(ch =>
+            (!filters.eye.isActive || ch.eye_color === filters.eye.color) &&
+            (!filters.gender.isActive || ch.gender === filters.gender.gen)
+          )
+          .map(({ name, url, gender, eye_color }) =>
+            <article key={name} className="flex flex-col items-center">
+              <Link href={`characters/${getIdFromUrl({ query: { url: url, q: "people" } })}`}>
+                <h2 className="text-2xl pl-2 text-primary font-bold">{name}</h2>
+                <img className="rounded-lg" src="/character.webp" alt={name} width={400} height={200} />
+                <div className="flex justify-around">
+                  {
+                    gender !== "unknown" &&
+                    gender !== "n/a" &&
+                    <div className="flex items-center gap-4">
+                      <p className="text-xl py-2">{gender}</p>
+                      <img className="h-4" src={`/${gender}.webp`} alt="eye" height={'auto'} />
+                    </div>
+                  }
+                  {
+                    eye_color !== "unknown" &&
+                    eye_color !== "n/a" &&
+                    <div className="flex items-center gap-4">
+                      <img className={`h-6 eye ${"eye-" + eye_color}`} src="eye.webp" alt="eye" height={'auto'} />
+                      <p className="text-xl py-2">{eye_color}</p>
+                    </div>
+                  }
+                </div>
+              </Link>
+            </article>
+          )
       }
       {/* You can replace the condition with 'next' property. getCharacters returns only 'results' prop */}
       {/* character.next !== null */}
