cbe63993337ac9d8af7ba1cb912b3c9c7fa8dd66
ferutizi <fer.utizi@gmail.com>
2024-05-12 20:29:14 -0300

    refactor(-): obtain searchParams and add Suspense react component

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -3,57 +3,60 @@ import { getCharacters } from "../api";
 import { getIdFromUrl } from "../hooks/useGetIdFromUrl";
 import { Metadata } from "next";
 import LoadMore from "./components/LoadMore";
-import { FiltersType } from "../types";
+import { Suspense } from "react";
+import Filters from "./components/Filters";
 
 export const metadata: Metadata = {
   title: `Star Wars Characters - Neowyze`,
   description: "Characters of Star Wars films",
 };
 
-export default async function Characters() {
+export default async function Characters({ searchParams }: { searchParams?: { gender?: string } }) {
   const characters = await getCharacters(1)
-
-  const filters: FiltersType = {
-    eye: { isActive: true, color: "blue" },
-    gender: { isActive: true, gen: "male" }
-  }
+  const gender = searchParams?.gender
 
   return (
-    <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 p-14">
-      {
-        characters
-          .filter(ch =>
-            (!filters.eye.isActive || ch.eye_color === filters.eye.color) &&
-            (!filters.gender.isActive || ch.gender === filters.gender.gen)
-          )
-          .map(({ name, url, gender, eye_color }) =>
-            <article key={name} className="flex flex-col items-center">
-              <Link href={`characters/${getIdFromUrl({ query: { url: url, q: "people" } })}`}>
-                <h2 className="text-2xl pl-2 text-primary font-bold">{name}</h2>
-                <img className="rounded-lg" src="/character.webp" alt={name} width={400} height={200} />
-                <div className="flex justify-around">
-                  {
-                    gender !== "unknown" &&
-                    gender !== "n/a" &&
-                    <div className="flex items-center gap-4">
-                      <p className="text-xl py-2">{gender}</p>
-                      <img className="h-4" src={`/${gender}.webp`} alt="eye" height={8} />
+    <>
+      <Filters />
+      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 p-14">
+        <Suspense key={gender}>
+          {
+            characters
+              .filter(ch =>
+                /* if isActive is false or filter match with character prop-- > map */
+                /* (!filters.eye.isActive || ch.eye_color === filters.eye.color) && */
+                (ch.gender === gender)
+              )
+              .map(({ name, url, gender, eye_color }) =>
+                <article key={name} className="flex flex-col items-center">
+                  <Link href={`characters/${getIdFromUrl({ query: { url: url, q: "people" } })}`}>
+                    <h2 className="text-2xl pl-2 text-primary font-bold">{name}</h2>
+                    <img className="rounded-lg" src="/character.webp" alt={name} width={400} height={200} />
+                    <div className="flex justify-around">
+                      {
+                        gender !== "unknown" &&
+                        gender !== "n/a" &&
+                        <div className="flex items-center gap-4">
+                          <p className="text-xl py-2">{gender}</p>
+                          <img className="h-4" src={`/${gender}.webp`} alt="eye" height={8} />
+                        </div>
+                      }
+                      {
+                        eye_color !== "unknown" &&
+                        eye_color !== "n/a" &&
+                        <div className="flex items-center gap-4">
+                          <img className={`h-6 eye ${"eye-" + eye_color}`} src="eye.webp" alt="eye" height={8} />
+                          <p className="text-xl py-2">{eye_color}</p>
+                        </div>
+                      }
                     </div>
-                  }
-                  {
-                    eye_color !== "unknown" &&
-                    eye_color !== "n/a" &&
-                    <div className="flex items-center gap-4">
-                      <img className={`h-6 eye ${"eye-" + eye_color}`} src="eye.webp" alt="eye" height={8} />
-                      <p className="text-xl py-2">{eye_color}</p>
-                    </div>
-                  }
-                </div>
-              </Link>
-            </article>
-          )
-      }
-      <LoadMore filters={filters} />
-    </section>
+                  </Link>
+                </article>
+              )
+          }
+        </Suspense>
+        {/*         <LoadMore filters={filters} /> */}
+      </section>
+    </>
   )
 }
