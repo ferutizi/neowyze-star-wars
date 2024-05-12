62f08f04351b2a313a94eaf0f04119cbe48904c3
ferutizi <fer.utizi@gmail.com>
2024-05-12 14:33:19 -0300

    add(-): background and text color in html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-background text-white">
       <body className={inter.className}>
         <Header />
         {children}
