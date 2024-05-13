2af18d5833e876eea17f54b5887935cdaaa08cd9
ferutizi <fer.utizi@gmail.com>
2024-05-13 14:56:31 -0300

    add(-): convert position to fixed header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-center p-2 md:pl-14">
+    <header className="flex fixed top-0 left-0 w-screen bg-background items-center justify-center p-2 md:pl-14 z-50">
       <div className="flex items-center max-w-[1920px] w-full">
         <Link href="/">
           <img src="/logo.webp" alt="Star Wars logo" width={120} height={100} />
