7e285978fdc941fdd60180f4ab8845cbd7eaf0db
ferutizi <fer.utizi@gmail.com>
2024-05-13 16:58:23 -0300

    refactor(-): Replace article with characterCard component

--- src/app/characters/components/LoadMore.tsx
+++ src/app/characters/components/LoadMore.tsx
@@ -4,8 +4,7 @@ import { useInView } from "react-intersection-observer"
 import { useState, useEffect } from "react"
 import { CharacterType, FiltersType } from "@/app/types"
 import { getCharacters } from "@/app/api"
-import Link from "next/link"
-import { getIdFromUrl } from "@/app/hooks/useGetIdFromUrl"
+import CharacterCard from "./CharacterCard"
 
 interface LoadMoreProps {
   gender?: string,
@@ -37,41 +36,8 @@ export default function LoadMore({ gender, eyeColor }: LoadMoreProps) {
             (!eyeColor || ch.eye_color.includes(eyeColor)) &&
             (!gender || ch.gender === gender)
           )
-          .map(({ name, url, gender, eye_color }) =>
-            <article key={name} className="flex flex-col items-center">
-              <Link href={`characters/${getIdFromUrl({ query: { url: url, q: "people" } })}`}>
-                <h2 className="text-2xl pl-2 text-primary font-bold">{name}</h2>
-                <img className="rounded-lg" src="/character.webp" alt={name} width={400} height={200} />
-                <div className="flex justify-around">
-                  {
-                    gender !== "unknown" &&
-                    gender !== "n/a" &&
-                    gender !== "none" &&
-                    <div className="flex items-center gap-4">
-                      <p className="text-xl py-2">{gender}</p>
-                      <img
-                        className="h-4"
-                        src={`/${gender === 'male' ? 'male' : ''}${gender === 'female' ? 'female' : ''}.webp`} alt={`gender ${gender}`}
-                        height={'auto'}
-                      />
-                    </div>
-                  }
-                  {
-                    eye_color !== "unknown" &&
-                    eye_color !== "n/a" &&
-                    <div className="flex items-center gap-4">
-                      <img
-                        className={`h-6 eye ${"eye-" + eye_color}`}
-                        src="eye.webp"
-                        alt="eye"
-                        height={'auto'}
-                      />
-                      <p className="text-xl py-2">{eye_color}</p>
-                    </div>
-                  }
-                </div>
-              </Link>
-            </article>
+          .map((character) =>
+            <CharacterCard character={character} />
           )
       }
       {/* You can replace the condition with 'next' property. getCharacters returns only 'results' prop */}
--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,11 +1,9 @@
-import './components/EyeColors.css'
-import Link from "next/link";
 import { getCharacters } from "../api";
-import { getIdFromUrl } from "../hooks/useGetIdFromUrl";
 import { Metadata } from "next";
 import LoadMore from "./components/LoadMore";
 import { Suspense } from "react";
 import Filters from "./components/Filters";
+import CharacterCard from './components/CharacterCard';
 
 export const metadata: Metadata = {
   title: `Star Wars Characters - Neowyze`,
@@ -35,31 +33,8 @@ export default async function Characters({
                 (!eye_color || ch.eye_color.includes(eye_color)) &&
                 (!gender || ch.gender === gender)
               )
-              .map(({ name, url, gender, eye_color }) =>
-                <article key={name} className="flex flex-col items-center">
-                  <Link href={`characters/${getIdFromUrl({ query: { url: url, q: "people" } })}`}>
-                    <h2 className="text-2xl pl-2 text-primary font-bold">{name}</h2>
-                    <img className="rounded-lg" src="/character.webp" alt={name} width={400} height={200} />
-                    <div className="flex justify-around">
-                      {
-                        gender !== "unknown" &&
-                        gender !== "n/a" &&
-                        <div className="flex items-center gap-4">
-                          <p className="text-xl py-2">{gender}</p>
-                          <img className="h-4" src={`/${gender}.webp`} alt="eye" height={8} />
-                        </div>
-                      }
-                      {
-                        eye_color !== "unknown" &&
-                        eye_color !== "n/a" &&
-                        <div className="flex items-center gap-4">
-                          <img className={`h-6 eye ${"eye-" + eye_color}`} src="eye.webp" alt="eye" height={8} />
-                          <p className="text-xl py-2">{eye_color}</p>
-                        </div>
-                      }
-                    </div>
-                  </Link>
-                </article>
+              .map((character) =>
+                <CharacterCard character={character} />
               )
           }
         </Suspense>
